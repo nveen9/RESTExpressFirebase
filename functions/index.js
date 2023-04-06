d77484700f06992cc2d9b9b23d7c8766f3ea8674
donut9 <naveenperera9@gmail.com>
2023-04-06 17:35:31 +0530

    first commit

---
+++ functions/index.js
@@ -0,0 +1,127 @@
+const functions = require("firebase-functions");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { auth, db } = require('./firebase');
+const { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } = require('firebase/auth');
+const { collection, doc, setDoc, getDoc, getDocs } = require('firebase/firestore');
+
+const app = express();
+
+app.use(bodyParser.json()); // Use body-parser middleware
+
+//CURRENT USER
+app.get('/isUser', async (req, res) => {
+    onAuthStateChanged(auth, user => {
+        if (!user) {
+            res.status(200).json({ message: "User not found!" });
+        } else {
+            const uid = user.uid;
+            res.status(200).json({ message: "User found!", uid });
+        }
+    })
+});
+
+//GET CURRENT USER DETAILS
+app.get('/getUser', async (req, res) => {
+    try {
+        const user = auth.currentUser.uid;
+        const docRef = doc(db, "users", user);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            res.status(200).json({ message: "User Details", "Document data: ": docSnap.data() });
+        } else {
+            res.status(500).json({ message: "No such data!" });
+        }
+    } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        res.status(500).json({ message: errorMessage, errorCode });
+    }
+});
+
+//GET ALL USERS DETAILS
+app.get('/getUsers', async (req, res) => {
+    try {
+        let documents = [];
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach((doc) => {
+            documents.push({
+                "DocID": doc.id,
+                "Data": doc.data()
+            });     
+        });
+        res.status(200).json(documents);
+    } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        res.status(500).json({ message: errorMessage, errorCode });
+    }
+});
+
+//SIGNUP
+app.post('/signup', async (req, res) => {
+    const { email, password } = req.body; // Extract data from the request body
+    console.log(email, password);
+
+    await createUserWithEmailAndPassword(auth, email, password)
+        .then(async userCredential => {
+            // User signed up successfully
+            const user = userCredential.user;
+            const uID = auth.currentUser.uid;
+            try {
+                const docRef = await setDoc(doc(db, "users", uID), {
+                    email: email,
+                    password: password,
+                    userID: uID
+                });
+                res.status(200).json({ message: "User signed up successfully", user });
+            } catch (error) {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                res.status(500).json({ message: errorMessage, errorCode });
+            }
+        })
+        .catch(error => {
+            // Error occurred while signing up user
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            res.status(500).json({ message: errorMessage, errorCode });
+        });
+});
+
+//LOGIN
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body; // Extract data from the request body
+    console.log(email, password);
+
+    await signInWithEmailAndPassword(auth, email, password)
+        .then(async userCredential => {
+            // User signed up successfully
+            const user = userCredential.user;
+            res.status(200).json({ message: "User logged in successfully", user });
+        })
+        .catch(error => {
+            // Error occurred while signing up user
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            res.status(500).json({ message: errorMessage, errorCode });
+        });
+});
+
+//LOGOUT
+app.post('/signout', async (req, res) => {
+    signOut(auth).then(() => {
+        res.status(200).json({ message: "Signout successfully" });
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        res.status(500).json({ message: errorMessage, errorCode });
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('This is AfterLook');
+});
+
+exports.app = functions.https.onRequest(app);
---
+++ index.js
@@ -0,0 +1,21 @@
+// const express = require('express');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+
+// app.use(cors(
+//     // {
+//     // origin: 'http://localhost:8080'
+//     // }
+// ));
+// app.use(bodyParser.json()); // Use body-parser middleware
+
+// app.get('/', (req, res) => {
+//     res.send('Home');
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server listening on port ${PORT}`);
+// });
